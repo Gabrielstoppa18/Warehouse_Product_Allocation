7283343f0b4522618a4c49b99c8ac496a5a3b095
Gabrielstoppa18 <gabriel.stoppa@gmail.com>
2022-09-12 11:17:05 -0300

    Update Strealit app

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import resolvedor
+import streamlit as st
+
+alg= resolvedor.SA()
+
+st.set_page_config(
+    page_title="Product Allocation",
+    page_icon="📦",
+)
+
+def _max_width_():
+    max_width_str = f"max-width: 1400px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+
+_max_width_()
+
+c30, c31, c32 = st.columns([2.5, 1, 3])
+
+with c30:
+    st.image("assets/image_3.png", width=400)
+    st.header("")
